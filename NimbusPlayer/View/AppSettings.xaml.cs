004dfe5b4c60228fc97d10ca368182974b02e2d0
Scott Commons <scott.lonewolf@gmail.com>
2016-02-03 11:48:06 +0000

    Fix for music directory setting
    Had an issue with saving the settings

--- NimbusPlayer/View/AppSettings.xaml.cs
+++ NimbusPlayer/View/AppSettings.xaml.cs
@@ -14,26 +14,30 @@ using System.Windows.Shapes;
 using NimbusPlayer.Properties;
 using System.ComponentModel;
 using System.Windows.Forms;
+using NimbusPlayer.ViewModel;
 
 namespace NimbusPlayer.View
 {
     /// <summary>
     /// Interaction logic for Window1.xaml
     /// </summary>
-    public partial class AppSettings : Window
+    public partial class AppSettings : INotifyPropertyChanged
     {
         public AppSettings()
         {
             InitializeComponent();
+            DataContext = this;
+            SelectedPath = Settings.Default.MusicFilePath;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             var browser = new System.Windows.Forms.FolderBrowserDialog();
-            browser.SelectedPath = Settings.Default["MusicFilePath"].ToString();
+            browser.SelectedPath = SelectedPath;
             DialogResult result = browser.ShowDialog();
             if (result == System.Windows.Forms.DialogResult.OK)
                 SelectedPath = browser.SelectedPath;
+
         }
 
         private string _selectedPath;
@@ -45,7 +49,13 @@ namespace NimbusPlayer.View
                 if (value != _selectedPath)
                 {
                     _selectedPath = value;
-                    Settings.Default["MusicFilePath"] = value;
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("SelectedPath"));
+                        Settings.Default.MusicFilePath = value;
+                        Settings.Default.Save();
+                        Settings.Default.Reload();
+                    }
                 }
             }
         }
